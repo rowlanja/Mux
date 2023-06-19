632a284f84ac40b376f62e0c877179b154fd5cac
j r <rowlanja@tcd.ie>
2023-06-19 19:41:03 +0100

    added transfers into contract

--- contract/src/msg.rs
+++ contract/src/msg.rs
@@ -4,17 +4,19 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct InstantiateMsg {
     pub admins: Vec<String>,
-}
-
-#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
-pub struct SetStateOfVaultMerkleTree {
-    pub vault: String,
+    pub donation_denom: String,
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub enum ExecuteMsg {
     AddMembers { admins: Vec<String> },
     Leave {},
+    Donate {},
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+pub struct SetStateOfVaultMerkleTree {
+    pub vault: String,
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
--- contract/target/debug/deps/contract.d
+++ contract/target/debug/deps/contract.d
@@ -1,5 +1,7 @@
 /home/rowla/mux/contract/target/debug/deps/libcontract.so: src/lib.rs src/contract.rs src/error.rs src/msg.rs src/state.rs
 
+/home/rowla/mux/contract/target/debug/deps/libcontract.rlib: src/lib.rs src/contract.rs src/error.rs src/msg.rs src/state.rs
+
 /home/rowla/mux/contract/target/debug/deps/contract.d: src/lib.rs src/contract.rs src/error.rs src/msg.rs src/state.rs
 
 src/lib.rs:
